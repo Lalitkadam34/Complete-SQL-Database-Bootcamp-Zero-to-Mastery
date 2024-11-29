084f829a7b97ebc92ec848a37fce2fa9b7fdccd3
Lalitkadam34 <112976801+Lalitkadam34@users.noreply.github.com>
2024-11-29 18:48:58 +0530

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -1,11 +1,13 @@
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
 
-/*
-* Write your query here
-*/
+SELECT fname, age
+FROM employees
+WHERE AGE BETWEEN 30 AND 50;
+
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
-/*
-* Write your query here
-*/
+
+SELECT avg(income)
+FROM emp
+WHERE age BETWEEN 20 AND 50
